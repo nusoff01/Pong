5927afd176c20ea43258f2a58985f9e50d4a2130
Nick Usoff <nusoff01@gmail.com>
2019-09-11 10:24:33 -0700

    Updates

---
+++ precache-manifest.ba997ea9d68b0b7bbbaf1be27a196fef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cc25ba71762171ffc239a07a1ae7fb4",
+    "url": "/Pong/index.html"
+  },
+  {
+    "revision": "f1de8cff585422416962",
+    "url": "/Pong/static/css/main.5be66dd5.chunk.css"
+  },
+  {
+    "revision": "78eb9931a0d1572c8467",
+    "url": "/Pong/static/js/2.7d70e74b.chunk.js"
+  },
+  {
+    "revision": "f1de8cff585422416962",
+    "url": "/Pong/static/js/main.20f24345.chunk.js"
+  },
+  {
+    "revision": "3269e6b58d21328184a9",
+    "url": "/Pong/static/js/runtime~main.6812f051.js"
+  }
+]);
